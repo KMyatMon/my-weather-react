14b94b36d28a18cd1c37715ad6bea5e3ecc95023
Khin Myat Mon <kmyatmon20@gmail.com>
2023-05-09 15:53:12 +0630

    code sourced
    inserted code All ok!

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="New York" />
+
+        <footer>
+          This project was coded by{" "}KMM
+          and is{" "}
+          <a
+            href="https://github.com/KMyatMon/my-weather-react"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+        </footer>
+      </div>
+    </div>
+  );
+}
