dcfee0f6ea638da14114f07ae53d31a9bd012efc
Khin Myat Mon <kmyatmon20@gmail.com>
2023-05-13 18:28:34 +0630

    final touch
    touched finally

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -27,8 +27,8 @@ export default function WeatherInfo(props) {
         </div>
         <div className="col-6">
           <ul>
-            <li>Humidity: {props.data.humidity}%</li>
-            <li>Wind: {props.data.wind} km/h</li>
+            <li>Humidity: <span className="text-danger">{props.data.humidity}%</span></li>
+            <li>Wind: <span className="text-danger">{props.data.wind} km/h</span></li>
           </ul>
         </div>
       </div>
